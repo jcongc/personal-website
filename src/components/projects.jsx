e8273ea6531e209dc69699d3884469ffc9b3e6f9
jason <111541945+jcongc@users.noreply.github.com>
2024-07-17 23:39:30 +1000

    Added link to MMA Data Scraper

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -5,12 +5,14 @@ const Projects = () => {
     return (
         <div className="proj-container" id="projects">
         <h1>Projects</h1>
-        <div className="proj-wrapper">
-            <img src="Logo-UFC.png" alt="UFC logo" className="projects-image"></img>
-            <div class="image-layer">
-                <div class="image-text">ESPN MMA Data Scraper</div>
+        <a href="https://github.com/jcongc/mma-data-scraper" target="_blank" rel="noopener noreferrer">
+            <div className="proj-wrapper">
+                <img src="Logo-UFC.png" alt="UFC logo" className="projects-image"></img>
+                <div class="image-layer">
+                    <div class="image-text">ESPN MMA Data Scraper</div>
+                </div>
             </div>
-        </div>
+        </a>
         <a href="https://github.com/jcongc/personal-website" target="_blank" rel="noopener noreferrer">
             <div className="proj-wrapper">
                 <img src="personalsite.png" alt="personal site" className="projects-image"></img>
