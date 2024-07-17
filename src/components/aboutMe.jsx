b0a8c1d0124c914bf2ebdd911a834ecfd56af124
jason <111541945+jcongc@users.noreply.github.com>
2024-07-18 00:34:51 +1000

    Edited about me

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -10,12 +10,6 @@ const AboutMe = () => {
             <p>I'm actively seeking industry opportunities as they offer invaluable experience and allows for an insight into skills outside the classroom.</p>
 
             <p>I've been obsessed with MMA since high school, and I saw this an opportunity to apply my current studies. I'm currently working on a data scraper to get MMA statistics for a fighter, and after that, hopefully making a fight predictor using machine learning models.</p>            
-            <p>I am currently studying these courses:</p>
-            <ul>
-                <li>MATH2601 - Higher Linear Algebra</li>
-                <li>MATH2901 - Higher Theory of Statistics</li>
-                <li>MATH2221 - Higher Differential Equations</li>
-            </ul>
         </div>
         <img src="me.png" alt="img of me" className="me-pic"/>
     </div>
